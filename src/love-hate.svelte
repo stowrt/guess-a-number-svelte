19f4791279d5b413761a20882b73c5274672ec67
vuelancer <selvakumardhivakar@gmail.com>
2020-08-08 02:28:51 +0530

    :art: Add love hate relationship - svelte app

---
+++ src/love-hate.svelte
@@ -0,0 +1,21 @@
+<script>
+	let sentence = "";
+</script>
+<h5>
+	Never propose me!
+</h5>
+<input type="text" bind:value={sentence}/>
+<hr/>
+{#each sentence.split(" ") as value}
+	{#if value.includes("love")}
+		<strike>
+			{value} 
+		</strike> ❌ - better hate me!
+  {:else if value.toLowerCase() === "hate"}
+		<strong> {value} ✅</strong>
+	{:else}
+		<p>
+			{value}
+		</p>
+	{/if}
+{/each}
