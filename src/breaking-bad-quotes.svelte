fb1c1737b0c350ea48888ebe09e1c80402429313
stowrt <68393880+stowrt@users.noreply.github.com>
2020-08-13 00:53:01 +0530

    Create breaking-bad-quotes.svelte

---
+++ src/breaking-bad-quotes.svelte
@@ -0,0 +1,35 @@
+<script>
+	let makeJoke = async ()=>{
+		let result = await fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes/100");
+		let jokes = await result.json();
+				console.log(jokes)
+		return jokes;
+	}
+</script>
+
+<main>
+	<button>Joke pls</button>
+	<hr/>
+	<hr/>
+	{#await makeJoke() then quotes}
+	{#each quotes as quote}
+  <h1>
+		{quote.quote}
+	</h1>
+	<h2>
+		{quote.author}
+	</h2>
+	<hr/>
+	
+	{/each}
+	{/await}
+</main>
+
+<style>
+	h1{
+		color: lightgreen;
+	}
+	h2{
+		color : #0055aa;
+	}
+</style>
